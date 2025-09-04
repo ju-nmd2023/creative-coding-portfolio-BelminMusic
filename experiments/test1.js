1222540509b12b83c22dec80cb5ec7a87ab38f62
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2025-09-04 17:10:39 +0200

    test

--- experiments/test1.js
+++ experiments/test1.js
@@ -1,1 +1,21 @@
- 
+ function setup() {
+    createCanvas(600, 600);
+ } 
+
+ function draw () {
+    background (255,255,255);
+
+    const originalY = 300;
+    const divider = 50;
+    beginShape ();
+    for (let x = 0; x < 600; x++) {
+        //const y = originalY + Math.random() * 100;
+        const y = originalY + noise(x / divider) * 100;
+        vertex(x, y);
+    }
+    endShape ();
+
+    noLoop();
+ }
+
+
