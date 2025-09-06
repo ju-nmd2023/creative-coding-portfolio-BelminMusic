d7b5e22247fda46c2336b2c8867405b5bacd6f8e
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2025-09-06 15:40:35 +0200

    Update

--- experiments/test3.js
+++ experiments/test3.js
@@ -1,14 +1,11 @@
  function setup() {
     createCanvas(600, 600);
-    frameRate(10);
  } 
 
  const size = 10;
  const divider = 20;
  const numRows = 60;
  const numCols = 60;
-
- let counter = 0;
 
  function draw () {
     background (255,255,255);
@@ -18,12 +15,13 @@
     //noiseSeed(0);
     for (let y = 0; y < numRows; y++) {
       for (let x = 0; x < numCols; x++) {
-         const value = noise(x /divider, y / divider, counter) * size;
+         const value = noise(x /divider, y / divider) * size;
          ellipse(size / 2 + x * size, size / 2 + y * size, value);
       }
     }
-    
-    counter +=0.1;
+
+
+    noLoop();
  }
 
 
