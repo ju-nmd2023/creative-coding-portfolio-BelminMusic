e8a13f18207a1fc4f491f0f945177f40aca249bf
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2025-09-08 14:22:30 +0200

    style update + new art

--- experiments/art11.js
+++ experiments/art11.js
@@ -4,7 +4,7 @@
  }
 
  const size = 100;
- const layers = 10;
+ const layers = 100;
 
  function getRandomValue(pos, variance) {
     return pos + map(Math.random(), 0, 1, -variance, variance);
@@ -25,7 +25,7 @@
         //colors for the lines
         stroke(random(255), random(255), random(255));
         //thicker lines
-        strokeWeight(2);
+        strokeWeight(10);
         
 
         beginShape();
@@ -42,9 +42,9 @@
     background(255, 140, 0);
 
     //drawLayers(100, 100, size, layers);
-    for (let y = 0; y < 10; y++) {
+    for (let y = 0; y < 20; y++) {
         for (let x = 0; x < 10; x++) {
-            drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
+            drawLayers(size / 4 + x * size, size / 4 + y * size, size, layers);
         }
     }
 
---
+++ experiments/art12.js
@@ -0,0 +1,52 @@
+ 
+ function setup() {
+    createCanvas(600, 600);
+ }
+
+ const size = 80;
+ const layers = 50;
+
+ function getRandomValue(pos, variance) {
+    return pos + map(Math.random(), 0, 1, -variance, variance);
+ }
+
+ function drawLayers(x, y, size, layers) {
+    //const half = size / 2;
+    const variance = size / 350;
+    noFill();
+    //rectMode(CENTER);
+    for (let i = 0; i < layers; i++) {
+        if (Math.random() > 0.8) {
+            continue;   
+        }
+        const s = (size / layers) * i;
+        const half = s / 2;
+
+        //colors for the lines
+        stroke(random(255), random(255), random(255));
+        //thicker lines
+        strokeWeight(2);
+        
+
+        beginShape();
+        vertex(getRandomValue(x - half, variance),getRandomValue(y - half, variance));
+        vertex(getRandomValue(x + half, variance),getRandomValue(y - half, variance));
+        vertex(getRandomValue(x + half,variance),getRandomValue(y + half, variance));
+        vertex(getRandomValue (x - half,variance),getRandomValue(y + half, variance));
+        endShape(CLOSE);
+        //rect(x - half, y - half, s, s);
+    }
+}
+
+ function draw() {
+    background(255, 140, 0);
+
+    //drawLayers(100, 100, size, layers);
+    for (let y = 0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {
+            drawLayers(size / 2 + x * size, size / 2 + y * size, size, layers);
+        }
+    }
+
+    noLooop();
+ }
