073c9aea3dcb694e3cc869020b33bfbe54b553de
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2025-09-11 11:49:38 +0200

    falling stars in the background

--- art21.js
+++ art21.js
@@ -1,5 +1,6 @@
 let angle;
 let leaves = [];
+let stars = [];
 
 class Leaf {
   constructor(x, y) {
@@ -26,6 +27,32 @@ class Leaf {
   }
 }
 
+class Star {
+  constructor() {
+    this.x = random(width);
+    this.y = random(-50, 0); // start above the canvas
+    this.size = random(2, 4);
+    this.speed = random(2, 5);
+  }
+
+  update() {
+    this.y += this.speed;
+  }
+
+  display() {
+    noStroke();
+    fill(60, 0, 255); // bright white star
+    ellipse(this.x, this.y, this.size);
+    // optional trail:
+    stroke(60, 0, 255, 150);
+    line(this.x, this.y, this.x, this.y - 10);
+  }
+
+  offScreen() {
+    return this.y > height;
+  }
+}
+
 function setup() {
   createCanvas(600, 600);
   colorMode(HSB);
@@ -35,24 +62,34 @@ function setup() {
 function draw() {
   background(0);
 
-  // Calculate the angle based on the mouse position, maximum 90 degrees
+  // ⭐ Falling stars in the background
+  if (random(1) < 0.05) { 
+    stars.push(new Star());
+  }
+  for (let i = stars.length - 1; i >= 0; i--) {
+    stars[i].update();
+    stars[i].display();
+    if (stars[i].offScreen()) {
+      stars.splice(i, 1);
+    }
+  }
+
+  // 🌳 Draw the tree
+  push();
   angle = (mouseX / width) * 90;
   angle = min(angle, 90);
 
-  // Start the tree from the bottom of the screen
   translate(width / 2, height);
 
-  // Draw the trunk
   stroke(0, 255, 255);
   line(0, 0, 0, -120);
 
-  // Move to the end of the trunk
   translate(0, -120);
 
-  // Start the recursive branching
   branch(120, 0);
+  pop();
 
-  // Update and draw falling leaves
+  // 🍂 Falling leaves
   for (let i = leaves.length - 1; i >= 0; i--) {
     leaves[i].update();
     leaves[i].display();
@@ -63,10 +100,8 @@ function draw() {
 }
 
 function branch(h, level) {
-  // Set the hue based on the recursion level
   stroke(level * 40 % 360, 255, 255);
 
-  // Each branch will be 2/3 the size of the previous one
   h *= 0.66;
 
   if (h > 2) {
@@ -86,14 +121,14 @@ function branch(h, level) {
     branch(h, level + 1);
     pop();
   } else {
-    // Draw a static leaf at the tip
+    // Static leaf at tip
     fill(120, 255, 255);
     noStroke();
     ellipse(0, 0, 6, 6);
 
     // Occasionally spawn a falling leaf
     if (random(1) < 0.01) {
-      let pos = getCanvasCoords(0, 0); // convert to absolute canvas coords
+      let pos = getCanvasCoords(0, 0);
       leaves.push(new Leaf(pos.x, pos.y));
     }
   }
